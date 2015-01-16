7ac1fa12a09a375451cb82fdb6b4b5f1ae7240f7
Stuber <yellowbike@me.com>
2015-01-16 10:59:36 -0600

    Newick Utilities, always rooting to "root"

--- SNP_analysis/bin/vcftofasta.sh
+++ SNP_analysis/bin/vcftofasta.sh
@@ -1148,7 +1148,7 @@ pwd
 cat *.fas >> fastaGroup.txt
 
 clustalw2 -OUTFILE=alignment.txt -RANGE=1,2 -OUTPUT=FASTA -INFILE=fastaGroup.txt & 
-/usr/local/bin/standard-RAxML-master/raxmlHPC-SSE3 -s fastaGroup.txt -n ${d} -m GTRCAT -p 12345 && nw_display -s -w 1000 -v 20 -b 'opacity:0' -i 'font-size:8' -l 'font-family:serif;font-style:italic' -d 'stroke-width:2;stroke:blue' RAxML_bestTree.${d} > ../${d}-tree.svg && inkscape -f ../${d}-tree.svg -A ../${d}-tree.pdf &
+/usr/local/bin/standard-RAxML-master/raxmlHPC-SSE3 -s fastaGroup.txt -n ${d} -m GTRCAT -p 12345 && nw_reroot RAxML_bestTree.${d} root | nw_display -s -w 1000 -v 20 -b 'opacity:0' -i 'font-size:8' -l 'font-family:serif;font-style:italic' -d 'stroke-width:2;stroke:blue' - > ../${d}-tree.svg && inkscape -f ../${d}-tree.svg -A ../${d}-tree.pdf &
 wait
 rm RAxML_parsimonyTree*
 for i in RAxML*Tree*; do mv $i ../${i}.nex; done
