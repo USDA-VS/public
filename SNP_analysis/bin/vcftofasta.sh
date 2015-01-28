e34d829e87861a46b0694a1af9ab3bffd8513d2b
Stuber <yellowbike@me.com>
2015-01-28 08:22:50 -0600

    root in RAxML and organized fixed

--- SNP_analysis/bin/vcftofasta.sh
+++ SNP_analysis/bin/vcftofasta.sh
@@ -1149,10 +1149,10 @@ echo "$d *********"
 pwd
 
 cat *.fas | sed '/root/{N;d;}' >> fastaGroup.txt
-#cat *.fas >> fastaGroup.txt
+cat *.fas >> RAxMLfastaGroup.txt
 
 clustalw2 -OUTFILE=alignment.txt -RANGE=1,2 -OUTPUT=FASTA -INFILE=fastaGroup.txt & 
-/usr/local/bin/standard-RAxML-master/raxmlHPC-SSE3 -s fastaGroup.txt -n ${d} -m GTRCAT -p 12345 && nw_reroot RAxML_bestTree.${d} root | nw_display -s -w 1000 -v 20 -b 'opacity:0' -i 'font-size:8' -l 'font-family:serif;font-style:italic' -d 'stroke-width:2;stroke:blue' - > ../${d}-tree.svg && inkscape -f ../${d}-tree.svg -A ../${d}-tree.pdf &
+/usr/local/bin/standard-RAxML-master/raxmlHPC-SSE3 -s RAxMLfastaGroup.txt -n ${d} -m GTRCAT -p 12345 && nw_reroot RAxML_bestTree.${d} root | nw_display -s -w 1000 -v 20 -b 'opacity:0' -i 'font-size:8' -l 'font-family:serif;font-style:italic' -d 'stroke-width:2;stroke:blue' - > ../${d}-tree.svg && inkscape -f ../${d}-tree.svg -A ../${d}-tree.pdf &
 wait
 rm RAxML_parsimonyTree*
 for i in RAxML*Tree*; do mv $i ../${i}.tree; done
@@ -1166,7 +1166,7 @@ mv joined2.txt ../$d.sortedTable.txt
 rm alignment.txt
 rm cleanedAlignment.txt
 #rm *.dnd
-rm fastaGroup.txt
+#rm fastaGroup.txt
 rm joined.txt
 rm references.txt
 
